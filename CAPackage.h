b2da271b40ed8b993e4988e67a087dfa403fd3ed
Brendon <brendonjkdingnew@gmail.com>
2021-03-28 20:46:56 +0800

    Add Settings Icon

---
+++ CAPackage.h
@@ -0,0 +1,4 @@
+@interface CAPackage : NSObject
+@property (readonly) CALayer * rootLayer; 
++(id)packageWithContentsOfURL:(id)arg1 type:(id)arg2 options:(id)arg3 error:(id*)arg4 ;
+@end
--- CCNoiseControlProvider.m
+++ CCNoiseControlProvider.m
@@ -1,5 +1,8 @@
 #import "CCNoiseControlProvider.h"
 #import "CCNoiseControl.h"
+#import "CAPackage.h"
+#import <ControlCenterUIKit/CCUICAPackageDescription.h>
+#import <objc/runtime.h>
 
 @implementation CCNoiseControlProvider
 
@@ -47,6 +50,32 @@
   else{
     return @"";
   }
+}
+
+- (UIImage*)settingsIconForModuleIdentifier:(NSString*)identifier
+{
+  CCUICAPackageDescription *description = [objc_getClass("CCUICAPackageDescription") descriptionForPackageNamed:@"CCNoiseControlIcon" inBundle:[NSBundle bundleForClass:[self class]]];
+  CAPackage* package = [CAPackage packageWithContentsOfURL:[description packageURL] type:@"com.apple.coreanimation-bundle" options:0 error:nil];
+  return [self imageFromLayer:[package rootLayer] flip:1];
+}
+
+//https://stackoverflow.com/questions/3454356/uiimage-from-calayer-in-ios
+//https://stackoverflow.com/questions/1135631/vertical-flip-of-cgcontext
+- (UIImage *)imageFromLayer:(CALayer *)layer flip:(BOOL)flip
+{
+  UIGraphicsBeginImageContextWithOptions(layer.frame.size, NO, 0);
+  CGContextRef context = UIGraphicsGetCurrentContext();
+  if(flip){
+    CGAffineTransform flipVertical = CGAffineTransformMake(
+        1, 0, 0, -1, 0, layer.frame.size.height
+    );
+    CGContextConcatCTM(context, flipVertical);
+  }
+  [layer renderInContext:context];
+  UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+  UIGraphicsEndImageContext();
+
+  return image;
 }
 
 @end
